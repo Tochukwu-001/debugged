82acb9f87b3f23cbbe7e0723db5d7b304d018e13
bigstan021 <investorstanlee@gmail.com>
2025-10-24 02:34:49 -0700

    message

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,18 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
-<<<<<<< HEAD
-import Navbar from "@/components/navbar";
 
-=======
+
 import Navbar from "@/components/Navbar";
 
 
->>>>>>> 92803e29c5fa6bfaaf69be8027408d7b159e51f6
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
-<<<<<<< HEAD
   title: "Debugged -A hub for fixes",
   description: "A platform for deelopers to share major b ugs ecountereed and thier respecive fixes.",
-=======
-  title: "Debugged - A hub for fixes",
-  description:
-    "A platform for developers to share major bugs encountered and their respective fixes.",
->>>>>>> 92803e29c5fa6bfaaf69be8027408d7b159e51f6
 };
 
 export default function RootLayout({ children }) {
@@ -30,11 +21,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${roboto.className} antialiased`}
       >
-<<<<<<< HEAD
         <Navbar/>
-=======
-        <Navbar />
->>>>>>> 92803e29c5fa6bfaaf69be8027408d7b159e51f6
         {children}
       </body>
     </html>

77cfaf7f9588fd224d2080ce7c0b604d9c719856
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-10-25 17:13:09 +0100

    marys updates

--- app/auth/signin/page.jsx
+++ app/auth/signin/page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { signIn } from "@/auth"
 
 const page = () => {
     return (

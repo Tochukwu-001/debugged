7845e52b5066b1deaff7aceb0ef3038ee6bbf42f
emmanuel <akamnonuemmanuel61@gmail.com>
2025-11-14 12:09:12 +0800

    message

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,21 +5,10 @@ import React, { useState } from "react";
 import { FiUser, FiLogOut } from "react-icons/fi";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { IoIosClose } from "react-icons/io";
-<<<<<<< HEAD
-import { useSession, signOut } from "next-auth/react"
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import { BiSolidLogOutCircle } from "react-icons/bi";
-
-
-
-=======
 import { useSession, signOut } from "next-auth/react";
 import Avatar from "@mui/material/Avatar";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
->>>>>>> 42f3d01560e03df74dfd46184787f220e83ad268
 
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
@@ -44,29 +33,18 @@ const Navbar = () => {
     },
     {
       name: "FAQs",
-<<<<<<< HEAD
-      url: "/faq"
-    }
-  ]
-  
-=======
       url: "/faq",
     },
   ];
 
->>>>>>> 42f3d01560e03df74dfd46184787f220e83ad268
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
-<<<<<<< HEAD
-    setAnchorEl(null);}
-=======
     setAnchorEl(null);
   };
->>>>>>> 42f3d01560e03df74dfd46184787f220e83ad268
 
   return (
     <nav className="flex items-center justify-between shadow-md px-5 py-3">
@@ -88,59 +66,6 @@ const Navbar = () => {
       </div>
 
       {session ? (
-<<<<<<< HEAD
-        // <img src={session?.user?.image} alt={session?.user?.name.slice(0, 1).toUpperCase()} />
-        
-         <div className='max-lg:ml-auto z-30'>
-      <button
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-        className='border-none outline-none'
-      >
-        <Avatar alt={session?.user?.name.slice(0, 1).toUpperCase()} src={session?.user?.image}/>
-      </button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        slotProps={{
-          list: {
-            'aria-labelledby': 'basic-button',
-          },
-        }}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>
-         <button onClick={() => signOut()} className='flex items-center gap-1 text-red-600 '>
-          <BiSolidLogOutCircle />
-          <span>logout</span>
-
-         </button>
-        </MenuItem>
-      </Menu>
-    </div>
-
-     ) : (
-      <Link href={"/auth/signin"} className='lg:flex hidden items-center gap-1 bg-blue-600 text-white px-6 
-      py-2 rounded-full'>
-        <FiUser className='text-xl' />
-        <p>My Account</p>
-      </Link>
-      )}
-
-      
-
-      <div className='lg:hidden text-2xl z-30 ml-3'>
-        <button onClick={()=> setShowNav(!showNav)}>
-          {
-            showNav ? <IoIosClose className='text-3xl' /> : <HiMenuAlt4 />
-          }
-=======
         // <img
         //   src={session?.user?.image}
         //   alt={session?.user?.name.slice(0, 1).toUpperCase()}
@@ -196,7 +121,6 @@ const Navbar = () => {
       <div className="lg:hidden text-2xl z-30 ml-3">
         <button onClick={() => setShowNav(!showNav)}>
           {showNav ? <IoIosClose className="text-3xl" /> : <HiMenuAlt4 />}
->>>>>>> 42f3d01560e03df74dfd46184787f220e83ad268
         </button>
       </div>
 
@@ -213,15 +137,6 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
-<<<<<<< HEAD
-        {
-          !session &&  <Link href={"/auth/signin"} className='flex items-center justify-center gap-1 bg-blue-600 text-white px-6 py-2 rounded-full'>
-          <FiUser className='text-xl' />
-          <p>My Account</p>
-        </Link>
-        }
-       
-=======
         {!session && (
           <Link
             href={"/auth/signin"}
@@ -231,15 +146,9 @@ const Navbar = () => {
             <p>My Account</p>
           </Link>
         )}
->>>>>>> 42f3d01560e03df74dfd46184787f220e83ad268
       </div>
     </nav>
   );
 };
 
-<<<<<<< HEAD
-export default Navbar
- 
-=======
 export default Navbar;
->>>>>>> 42f3d01560e03df74dfd46184787f220e83ad268
